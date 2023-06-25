1513f11b472417b5cb0359746b8379226b3298dd
zhaiduting <zhaiduting@163.com>
2023-06-25 05:49:59 +0000

    custom hook

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
 import { useState, useEffect, useRef } from 'react';
+import { useFadeIn } from './useFadeIn.js';
 
 function Welcome() {
   const ref = useRef(null);
 
-  useEffect(() => {
-    const duration = 1000;
-    const node = ref.current;
-
-    let startTime = performance.now();
-    let frameId = null;
-
-    function onFrame(now) {
-      const timePassed = now - startTime;
-      const progress = Math.min(timePassed / duration, 1);
-      onProgress(progress);
-      if (progress < 1) {
-        // We still have more frames to paint
-        frameId = requestAnimationFrame(onFrame);
-      }
-    }
-
-    function onProgress(progress) {
-      node.style.opacity = progress;
-    }
-
-    function start() {
-      onProgress(0);
-      startTime = performance.now();
-      frameId = requestAnimationFrame(onFrame);
-    }
-
-    function stop() {
-      cancelAnimationFrame(frameId);
-      startTime = null;
-      frameId = null;
-    }
-
-    start();
-    return () => stop();
-  }, []);
+  useFadeIn(ref, 1000);
 
   return (
     <h1 className="welcome" ref={ref}>
